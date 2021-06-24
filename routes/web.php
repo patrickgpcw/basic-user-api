9c5a9b796f0a8ce45f68abc9d39e41b6eef17c1f
patrickgpcw <patrick.surfista@hotmail.com>
2021-06-24 13:41:57 -0300

    Criação do README.MD Pequenas melhorias no test

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Support\Facades\Salt;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +14,5 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', function () {
-    dd(Salt::generate());
     return view('welcome');
 });
